b87ddca231da7c0c25a35280e24932f1ee1ffa93
Cuverty <45130672+Cuverty@users.noreply.github.com>
2024-06-17 14:13:06 +0200

    Added 4 new parks and 9 new POIs

--- IdzDoParkuDjango/populate_db.py
+++ IdzDoParkuDjango/populate_db.py
@@ -31,7 +31,134 @@ def populate():
         description="Piękny taras widokowy, skąd można podziwiać staw i otaczającą przyrodę.",
         latitude=51.0704,
         longitude=17.0217,
-        qr_code="QR_CODE_LINK",
+        qr_code="poludniowytaras",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    # Dodaj Park Szczytnicki
+    park_szczytnicki = Park.objects.create(
+        park_name="Park Szczytnicki",
+        location="Wrocław, Poland",
+        description="Leśny park ze słynnym ogrodem japońskim zaprojektowanym w 1913 r. i kilkakrotnie później przebudowywanym."
+    )
+
+    POI.objects.create(
+        park=park_szczytnicki,
+        name="Aleja Sakur",
+        description="Wyjątkowy sad z oryginalnymi drzewkami japońskich wiśni",
+        latitude=51.1141, 
+        longitude=17.0783,
+        qr_code="szczytsakur",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    POI.objects.create(
+        park=park_szczytnicki,
+        name="Pomnik Schillera",
+        description="Pomnik przedstawiający niemieckiego literata Friedricha Schillera",
+        latitude=51.1133,
+        longitude=17.0831,
+        qr_code="szczytschill",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    POI.objects.create(
+        park=park_szczytnicki,
+        name="Altana",
+        description="Jest to rekonstrukcja miedzianej altany, która wcześniej znajdowała się w tym miejscu. ",
+        latitude=51.1139,
+        longitude=17.0885,
+        qr_code="szczytaltana",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    # Dodaj Park Juliusza Słowackiego
+    park_slowackiego = Park.objects.create(
+        park_name="Park Juliusza Słowackiego",
+        location="Wrocław, Poland",
+        description="Spokojny park miejski pełen posągów i pomników wojennych nazwany na cześć polskiego poety romantycznego."
+    )
+
+    POI.objects.create(
+        park=park_slowackiego,
+        name="Pomnik Juliusza Słowackiego",
+        description="Pomnik upamiętniający polskiego wieszcza narodowego – Juliusza Słowackiego.",
+        latitude=51.1093,
+        longitude=17.0452,
+        qr_code="slowakpomnik",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    POI.objects.create(
+        park=park_slowackiego,
+        name="Rzeźba \"Oczekiwanie\"",
+        description="Przedstawia dwa duże fotele, mocno stylizowane, sprawiające wrażenie miękkich i wygodnych. Na jednym siedząca postać młodej kobiety w okularach i sukience.",
+        latitude=51.1101,
+        longitude=17.0476,
+        qr_code="slowakrzezba",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    # Dodaj Park Zachodni
+    park_zachodni = Park.objects.create(
+        park_name="Park Zachodni",
+        location="Wrocław, Poland",
+        description="Zielony park miejski z placami zabaw dla dzieci i pozostałościami po fortyfikacjach z czasów I wojny światowej."
+    )
+
+    POI.objects.create(
+        park=park_zachodni,
+        name="Schron Piechoty I.R.-10",
+        description="Schron piechoty typu magazynowego wybudowany w latach 1895-96.",
+        latitude=51.1318,
+        longitude=16.9675,
+        qr_code="zachodnischron",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    POI.objects.create(
+        park=park_zachodni,
+        name="Pomnik Anioła",
+        description="Figura anioła z urną we wrocławskim Parku Zachodnim przypomina, że utworzony w latach 1905-1910 park był powiązany z cmentarzami.",
+        latitude=51.1316,
+        longitude=16.9769,
+        qr_code="zachodnianiol",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    # Dodaj Park Wschodni
+    park_wschodni = Park.objects.create(
+        park_name="Park Wschodni",
+        location="Wrocław, Poland",
+        description="Położony nad rzeką rozległy park z terenami trawiastymi i kładkami – malownicze miejsce na spacery."
+    )
+
+    POI.objects.create(
+        park=park_wschodni,
+        name="Mała \"Niagara\"",
+        description="Kaskada wodna na wschodnim krańcu parku",
+        latitude=51.0858,
+        longitude=17.0904,
+        qr_code="wschodnipomnik",
+        additional_info_link="https://example.com",
+        score_worth=10
+    )
+
+    POI.objects.create(
+        park=park_wschodni,
+        name="Megafony",
+        description="Instalacja akustyczna do wzmacniania dźwięków krajobrazu, składająca się oryginalnie z trzech, aktualnie – ze względów konserwatorskich – z dwóch olbrzymich drewnianych megafonów, zbudowanych przez studentów architektury wnętrz Estońskiej Akademii Sztuki",
+        latitude=51.0866,
+        longitude=17.0866,
+        qr_code="wschodnimegafon",
         additional_info_link="https://example.com",
         score_worth=10
     )
